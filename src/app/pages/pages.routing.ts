859e3ff1c8fa82cad6f5e52a8c728df44daa4b2d
Moise <murhi46@gmail.com>
2021-11-01 09:51:03 +1000

    Update

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -16,53 +16,7 @@ export const Pagesroutes: Routes = [
   { path: "recipes", component: RecipesComponent },
 
   
-  // {
-  //       path: '', component: PagesComponent, children: [
-  //         { path: '', redirectTo: 'board', pathMatch: 'full' },
-  //         {  path: "board", component: BoardComponent },
-  //         { path: "weekly-menu", component: WeeklyMenuComponent },
-  //         { path: "recipes", component: RecipesComponent },
-  //       ]
-  //     }
     ];
 
 
 
-
-
-
-
-
-
-//   import { NgModule } from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
-// import { CommonModule } from '@angular/common';
-
-// import { BoardComponent } from "./board/board.component";
-// import { RecipesComponent } from "./recipes/recipes.component";
-// import { WeeklyMenuComponent } from "./weekly-menu/weekly-menu.component";
-// import { PagesComponent } from './pages.component';
-
-
-
-// export const Pagesroutes: Routes = [
-//     {
-//         path: '', component: PagesComponent, children: [
-//           { path: '', redirectTo: 'board', pathMatch: 'full' },
-//           {  path: "board", component: BoardComponent },
-//           { path: "weekly-menu", component: WeeklyMenuComponent },
-//           { path: "recipes", component: RecipesComponent },
-//         ]
-//       }
-//     ];
-
-
-// @NgModule({
-  
-//     imports: [
-//         CommonModule,
-//         RouterModule.forChild(Pagesroutes)
-//     ],
-//     exports: [RouterModule]
-//   })
-//   export class PagesRoutingModule { }
